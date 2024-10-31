324d87481cf5ba5843f9612c4f519a7ac8bf4680
unknown <shekhubaidraza76@gmail.com>
2024-10-31 11:45:48 +0500

    make it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
   return(
     <div>
-      <h1 style={{textAlign:"center",margin:"300px", textShadow:" 2px 2px 4px red"}}>Hello World</h1>
+      <h1 style={{textAlign:"center",marginTop:"300px", textShadow:" 2px 2px 4px red"}}>Hello World</h1>
     </div>
   )
 }
